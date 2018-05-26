b1b69b35967cf68b2a56b244da549090df8a19a8
Rob Fitzgerald <robfitzgerald@gmail.com>
2018-05-26 15:22:39 -0600

    all TerminationCriterion are serializable, so we can share a common criterion between Spark executors as a broadcast variable.

--- src/main/scala/cse/bdlab/fitzgero/mcts/core/terminationcriterion/TerminationCriterion.scala
+++ src/main/scala/cse/bdlab/fitzgero/mcts/core/terminationcriterion/TerminationCriterion.scala
@@ -2,8 +2,7 @@ package cse.bdlab.fitzgero.mcts.core.terminationcriterion
 
 import cse.bdlab.fitzgero.mcts.tree._
 
-trait TerminationCriterion [S,A,N <: MonteCarloTree[S,A,_,_,_,_]] {
+trait TerminationCriterion [S,A,N <: MonteCarloTree[S,A,_,_,_,_]] extends Serializable {
   def init(): Unit
   def withinComputationalBudget(monteCarloTree: N): Boolean
 }
-
